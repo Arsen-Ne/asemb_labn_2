484cf39c066c0ed0559c1c139062f00146badb3b
Арсений Несытых <arsen.nesytykh@gmail.com>
2022-03-16 00:44:25 +0300

    v2 - sum between min and max

--- asemb_labn_2/asemb_labn_2.cpp
+++ asemb_labn_2/asemb_labn_2.cpp
@@ -19,51 +19,52 @@ int main()
     int max = 0;
     int min = 0;
     int n1 = n - 1;
-    _asm{
-        mov ecx,n1
-        mov ebx,b //занесение адреса массива b
-        add ebx,4    
-        mov esi,b // min address
-        mov eax,[esi] // min value
-        mov edi,b // max address
-        mov edx,eax // max value
-    l1 :
-        cmp eax, [ebx] // сравниваем минимум с текущим
-        jle e1 // если (eax < [ebx] == true) меньше 1й, то jmp to e1
-        mov esi,ebx
-        mov eax,[ebx]
-        jmp e2
-    e1:
-        cmp edx, [ebx]
-            jge e2
-            mov edi, ebx
-            mov edx, [ebx]
-            e2 :
-            add ebx, 4
-            loop l1
+	_asm {
+		mov ecx, n1
+		mov ebx, b;//занесение адреса массива b
+		add ebx, 4
+		mov esi, b; // min address
+		mov eax, [esi]; // min value
+		mov edi, b; // max address
+		mov edx, eax; // max value
+	l1 :
+		cmp eax, [ebx]; // сравниваем минимум с текущим
+		jle e1;// если (eax < [ebx] == true) меньше 1й, то jmp to e1
+		mov esi, ebx
+		mov eax, [ebx]
+		jmp e2
+	e1 :
+		cmp edx, [ebx]
+		jge e2
+		mov edi, ebx
+		mov edx, [ebx]
+	e2 :
+		add ebx, 4
+		loop l1
 
-            mov max, edx
-            mov min, eax
+		mov max, edx
+		mov min, eax
 
-            xor eax, eax
+		xor eax, eax
 
-            cmp esi, edi
-            jl e3
+		cmp esi, edi
+		jl e3
 
-            mov ebx, edi            
-            mov ecx, esi
-            jmp l2
-    e3 : 
-        mov ebx, esi
-        mov ecx, edi    
+		mov ebx, edi
+		mov ecx, esi
+		jmp l2s
+	e3 :
+		mov ebx, esi
+		mov ecx, edi
 
-    l2:
-        add eax,[ebx]
-        add ebx,4
-        cmp ebx,ecx
-        jle l2
-
-        mov sum,eax
+	l2s :
+		add ebx, 4
+		cmp ebx, ecx
+		jge l2e
+		add eax, [ebx]
+		jmp l2s
+	l2e :
+		mov sum, eax
     }
     cout << "min=" << min << endl;
     cout << "max=" << max << endl;
